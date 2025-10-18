b765a7e8372123315053bf0a4d7faa1517efaadc
ANUJT65 <anujtadkase@gmail.com>
2025-10-18 23:45:55 +0530

    listing model completed

---
+++ Model/gemini_default.py
@@ -0,0 +1,14 @@
+from google import genai
+from google.genai import types
+
+client = genai.Client(api_key="AIzaSyDiUMs4sIAdOk09006hS7DcY79DZh53_M4")
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash", 
+    contents="Explain how AI works ",
+    config=types.GenerateContentConfig(
+        max_output_tokens=400,
+        thinking_config=types.ThinkingConfig(thinking_budget=0)
+    )
+)
+print(response.text)
